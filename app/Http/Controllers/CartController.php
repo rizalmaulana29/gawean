aa63fb1c7e484b8afaa50d2fc69471e40127eead
sandi@niagateknologi.net <Sukses100>
2019-12-12 16:40:24 +0700

    update Cart Part 8

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -79,13 +79,12 @@ class CartController extends Controller
       foreach ($req['id_produk_harga'] as $key => $id_produk) {
   
           $order = new Order;
-          $order->id_ra_payment = $result[1]->id;
-          $order->id_order = $result[1]->id_transaksi;
+          $order->id_ra_payment = $result[2]->id;
+          $order->id_order = $result[2]->id_transaksi;
           $order->id_kantor = $request->input('id_kantor');
           $order->ra_produk_harga_id = $id_produk;
-          // $order->id_via_bayar = 1;
-          $order->id_pelanggan = Kontak::where('id',$result[1]->id)->where('status','Customer')->first();
-          $order->id_anak = Kontak::where('id',$result[1]->id)->where('status','Anak')->first();
+          $order->id_pelanggan = Kontak::where('id',$result[1]->id)->where('status','customer')->first();
+          $order->id_anak = Kontak::where('id',$result[1]->id)->where('status','anak')->first();
           // $order->id_via_bayar = 1;
           $order->id_agen = $request->input('agen');
           $order->coa_debit = $request->input('coa'); 

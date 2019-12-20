dca3be5c535f9ed7d3306b6f73308dd6fd564982
sandi@niagateknologi.net <Sukses100>
2019-12-20 09:38:41 +0700

    update mail to mail RA 1.7

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -139,7 +139,7 @@ class CartController extends Controller
       $to_address = $request->input('email');
       $transdata = Payment::where('id',$result[2]->id)->first();
       $orderdata = Order::where('id_order',$result[2]->id_transaksi)->get();
-      dd($transdata->id_transaksi);
+      // dd($transdata->id_transaksi);
       $nama = $req['nama'][0];
       $alamat = $req['alamat'];
       $kokec = $req['kota']; $req['kecamatan'];

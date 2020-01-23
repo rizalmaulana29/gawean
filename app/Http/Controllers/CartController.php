20ae17063ea832c926dec19ea57979692ebaf6bf
sandi@niagateknologi.net <Sukses100>
2020-01-23 13:21:03 +0700

    cors handling test

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -34,7 +34,7 @@ class CartController extends Controller
     
     public function cart(Request $request){
       $req = $request->all();
-      dd($req);
+      // dd($req);
       $now = Carbon::now();
       $expired_at = Carbon::now()->addHour(24);
 
@@ -107,7 +107,7 @@ class CartController extends Controller
         $result[1]->ra_payment_id  = $result[2]->id;
         $result[1]->id_order       = $result[2]->id_transaksi;
 
-        // dd($request->file('foto_anak')[$key]);
+        dd($request->file('foto_anak'));
         if ($request->file('foto_anak')[$key]) {
           $image = $request->file('foto_anak')[$key];
           $imageName = 'raqiqah'. rand(1,1000). '.' . $image->getClientOriginalExtension();

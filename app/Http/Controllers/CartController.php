62f4bad5187db48f5a0987ae323bcaccbda12a5e
sandi@niagateknologi.net <Sukses100>
2020-01-23 17:15:24 +0700

    tester api kata rivan 2

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -34,7 +34,7 @@ class CartController extends Controller
     
     public function cart(Request $request){
       $req = $request->all();
-      dd($req);
+      vardump($req);
       $now = Carbon::now();
       $expired_at = Carbon::now()->addHour(24);
 

1bb2c553a925b98b2cb9db5fa0be6167a6d1cd34
Dynasti Madani Faz <dynasti@niagateknologi.net>
2020-01-31 14:40:18 +0700

    cors

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api','middleware' => 'cors'], function () use ($router) {
+$router->group(['prefix' => 'api'], function () use ($router) {
 	$router->get('harga', 	['uses' =>'HargaController@show']);
 	$router->get('code',	['uses' =>'CodeController@unicCode']);
 	$router->post('cart', 	['uses' => 'CartController@cart']);

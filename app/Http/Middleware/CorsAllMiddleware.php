5d048fc7f5a72507c41b2662154f5111751ee32b
sandi@niagateknologi.net <Sukses100>
2020-08-27 14:54:35 +0700

    update cors

--- app/Http/Middleware/CorsAllMiddleware.php
+++ app/Http/Middleware/CorsAllMiddleware.php
@@ -17,36 +17,29 @@ class CorsAllMiddleware
     {
 
         $http_origin = (isset($_SERVER['HTTP_ORIGIN']))?$_SERVER['HTTP_ORIGIN']:"";
-        
+
         $headers = [
-            'Access-Control-Allow-Origin'      => 'https://order.rumahaqiqah.co.id',
+
             'Access-Control-Allow-Methods'     => 'POST, GET',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
 
+        if ($http_origin == "https://order.rumahaqiqah.co.id/")
+            { $headers["Access-Control-Allow-Origin"] = $http_origin;}
+        else
+            {$headers["Access-Control-Allow-Origin"] = "https://dev.rumahaqiqah.co.id";}
+
         if ($request->isMethod('OPTIONS')) {
             return response()->json('{"method":"OPTIONS"}', 200, $headers);
         }
 
-        $IlluminateResponse = 'Illuminate\Http\Response';
-        $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
-
         $response = $next($request);
-        if($response instanceof $IlluminateResponse) {
-            foreach ($headers as $key => $value) {
-                $response->header($key, $value);
-            }
-            return $response;
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
         }
-        
-        if($response instanceof $SymfonyResopnse) {
-            foreach ($headers as $key => $value) {
-                $response->headers->set($key, $value);
-            }
-            return $response;
-        }
+
         return $response;
     }
 }

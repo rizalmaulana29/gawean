327ee2c69f8dcfff23852266535662c9beddf87b
sandi@niagateknologi.net <Sukses100>
2020-08-27 15:12:12 +0700

    update cors

--- app/Http/Middleware/CorsAllMiddleware.php
+++ app/Http/Middleware/CorsAllMiddleware.php
@@ -16,20 +16,20 @@ class CorsAllMiddleware
     public function handle($request, Closure $next)
     {
 
-        $http_origin = (isset($_SERVER['HTTP_ORIGIN']))?$_SERVER['HTTP_ORIGIN']:"";
+        // $http_origin = (isset($_SERVER['HTTP_ORIGIN']))?$_SERVER['HTTP_ORIGIN']:"";
 
         $headers = [
-
+            'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
 
-        if ($http_origin == "https://order.rumahaqiqah.co.id/")
-            { $headers["Access-Control-Allow-Origin"] = $http_origin;}
-        else
-            {$headers["Access-Control-Allow-Origin"] = "https://dev.rumahaqiqah.co.id";}
+        // if ($http_origin == "https://order.rumahaqiqah.co.id/")
+        //     { $headers["Access-Control-Allow-Origin"] = $http_origin;}
+        // else
+        //     {$headers["Access-Control-Allow-Origin"] = "https://dev.rumahaqiqah.co.id";}
 
         if ($request->isMethod('OPTIONS')) {
             return response()->json('{"method":"OPTIONS"}', 200, $headers);

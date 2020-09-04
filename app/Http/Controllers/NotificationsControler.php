4ae2903d20217014398040717e2052034feda689
mochiraven <rivan@niagateknologi.net>
2020-09-04 14:25:49 +0700

    lain Value tapi pluck

--- app/Http/Controllers/NotificationsControler.php
+++ app/Http/Controllers/NotificationsControler.php
@@ -44,7 +44,7 @@ class NotificationsController extends Controller
 
 
         // $payment    = Payment::where('id_transaksi',$referenceNo)->first();
-        $paymeth    = Paymeth::where('parent_id','<','5')->value('id');
+        $paymeth    = Paymeth::where('parent_id','<=','5')->pluck('id');
         dd($paymeth);
         #Get DATA Transaksi yg belum kirim Email
         $listTransaksi = Payment::select('id_transaksi','tgl_transaksi','id_donatur')

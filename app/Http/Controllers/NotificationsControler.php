8fe110aa66bf175efd44ec545b940c8e7330e6e6
mochiraven <rivan@niagateknologi.net>
2020-09-04 14:46:14 +0700

    test var_dump

--- app/Http/Controllers/NotificationsControler.php
+++ app/Http/Controllers/NotificationsControler.php
@@ -46,7 +46,7 @@ class NotificationsController extends Controller
         // $payment    = Payment::where('id_transaksi',$referenceNo)->first();
         $paymeth    = Paymeth::where('parent_id','<=','5')->pluck('id');
         #Get DATA Transaksi yg belum kirim Email
-        var_dump($paymeth->items);
+        var_dump($paymeth);
         $listTransaksi = Payment::select('id_transaksi','tgl_transaksi','email','id','id_parent')
             ->where('status','!=','paid')
             ->where('tgl_transaksi','<', $dateNow)

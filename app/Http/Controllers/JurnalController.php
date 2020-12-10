b629943dcbc4550ccc86cf6699b124dd2c265f4e
sandi@niagateknologi.net <Sukses100>
2020-12-10 21:12:36 +0700

    api jurnal id sales order

--- app/Http/Controllers/JurnalController.php
+++ app/Http/Controllers/JurnalController.php
@@ -43,7 +43,7 @@ class JurnalController extends Controller
                                  // ->get();
       $createCustomer = $this->CreateCustomer($getDataTransaksi);
       var_dump($createCustomer);
-      dd($createCustomer[0]);
+      dd($createCustomer->status);
       die;
       if ($createCustomer->status == "success") {
         $salesOrder = $this->SalesOrder($getDataTransaksi,$createCustomer);

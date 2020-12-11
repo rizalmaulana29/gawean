d7f4e326c5f48fba45d91eeccdac21c562009c06
sandi@niagateknologi.net <Sukses100>
2020-12-11 08:57:02 +0700

    testing sales order

--- app/Http/Controllers/JurnalController.php
+++ app/Http/Controllers/JurnalController.php
@@ -146,9 +146,8 @@ class JurnalController extends Controller
       $data_produk = "";
       foreach ($dataOrder as $key => $order) {
 
-        var_dump($order['id_produk_harga']);
         $ending              = (count($dataOrder) == $countData)?"":",";
-        $produk_harga        = Harga::where('id',$order['id_produk_harga'])->value('jurnal_product_id');
+        $produk_harga        = Harga::where('id',$order['ra_produk_harga_id'])->value('jurnal_product_id');
         $data_produk        .= "[ quantity"."=>". $order['quantity'].", rate"."=>". $order['harga'].",product_id"."=>". $produk_harga."]".$ending;
         $countData++;
       }

ce8d39875fd3951aaa027720eacbe48c404b4d24
sandi@niagateknologi.net <Sukses100>
2021-01-04 15:05:16 +0700

    push all hasil perbaikan

--- app/Http/Controllers/JurnalDeleteController.php
+++ app/Http/Controllers/JurnalDeleteController.php
@@ -88,7 +88,7 @@ class JurnalDeleteController extends Controller
       if ($err) {
         $response = array("status"=>false,"message"=>"cURL Error #:" . $err);
       } else {
-        dd($response->status());
+        var_dump(http_response_code());
         $response = array("status"=>true,"message"=> "data berhasil di hapus".$response);
       }
 

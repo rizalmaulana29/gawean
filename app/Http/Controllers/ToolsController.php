68a5a87622dbc6203d8370726b1e67cc05b05cc1
sandi@niagateknologi.net <Sukses100>
2021-01-20 17:46:14 +0700

    API Perhitungan Tools

---
+++ app/Http/Controllers/ToolsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Harga;
+use App\Kantor;
+use App\Produk;
+use App\Payment;
+use App\Order;
+use App\StockTool;
+
+class ToolsController extends Controller
+{
+    
+    public function toolsMath (Request $request) //$id_kantor,$id_transaksi
+    {
+        
+        $getIdProduk = Order::where('id_order',$request['id_transaksi'])->where('id_produk_parent','22')->value('ra_produk_harga_id');
+        $qty = Order::where('id_order',$request['id_transaksi'])->where('id_produk_parent','22')->value('quantity');
+        $getNamaProduk = Harga::where('id',$getIdProduk)->value('nama_produk');
+
+        if ($getNamaProduk == 'Ordinary') {
+
+            $qtyBento = StockTool::where('id_kantor',$request['id_kantor'])->where('keterangan','Bento')->value('jumlah_stock');
+            $updateBento = StockTool::where('id_kantor',$request['id_kantor'])->where('keterangan','Bento')->update(['jumlah_out' => $qty,'jumlah_stock' => $qtyBento - $qty]);
+            $getQtyTool = StockTool::where('id_kantor',$request['id_kantor'])->where('keterangan','Perlengkapan')->get();
+            foreach ($getQtyTool as $key => $dataTool) {
+                $updateStock = StockTool::where('id_kantor',$request['id_kantor'])->where('keterangan','Perlengkapan')->update(['jumlah_out' => $qty,'jumlah_stock' => $dataTool['jumlah_stock'] - $qty]);
+            }
+            if ($updateBento && $updateStock) {
+                $response = "Data Bento berhasil di update";
+            } else {
+                $response = "Data Bento gagal di update";
+            } 
+            
+        } else {
+            $getQtyTool = StockTool::where('id_kantor',$request['id_kantor'])->whereIn('keterangan', ['Perlengkapan', 'Box'])->get();
+            foreach ($getQtyTool as $key => $dataTool) {
+                $updateStock = StockTool::where('id_kantor',$request['id_kantor'])->whereIn('keterangan', ['Perlengkapan', 'Box']))->update(['jumlah_out' => $qty,'jumlah_stock' => $dataTool['jumlah_stock'] - $qty]);
+            }
+            if ($updateBento && $updateStock) {
+                $response = "Data Box berhasil di update";
+            } else {
+                $response = "Data Box gagal di update";
+            } 
+        }
+
+        return response()->json(["status"       => true,
+                                     "Data Response"=> $response
+                                    ],200);
+        
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 	$router->post('TestWA', 	['uses' => 'CartDevController@sendWa']);
 
 	$router->post('TestNotif', 	['uses' => 'CartDevController@notifTransaksi']);
+	$router->post('stockTool', 	['uses' => 'ToolsController@toolsMath']);
 
 	$router->group(['middleware' => 'all.cors'], function () use ($router) {
 		$router->post('check/number', 	['uses' => 'CartController@checkNumber']);

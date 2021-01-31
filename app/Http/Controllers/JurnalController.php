1b09fb04a2336b66f974411aefd5a839bac8f78e
sandi@niagateknologi.net <Sukses100>
2021-01-31 08:37:11 +0700

    test dev all kantor dan ginger

--- app/Http/Controllers/JurnalController.php
+++ app/Http/Controllers/JurnalController.php
@@ -26,7 +26,7 @@ class JurnalController extends Controller
       $endDate = Carbon::now()->endOfMonth();
       $start = Carbon::yesterday()->addHour(1)->toDateTimestring();
 
-      $getDataTransaksi = Payment::select('ra_payment_dua.id_transaksi','nama_customer','alamat','tgl_transaksi',
+      $getDataTransaksi = Payment::select('id_transaksi','nama_customer','alamat','ra_payment_dua.tgl_transaksi',
                                           'ra_payment_dua.id_payment_method','tgl_kirim','hp','email','ra_payment_dua.id_kantor',
                                           'ra_payment_dua.id_agen','nominal_diskon','nominal_bayar','nominal_total','jenis','tgl',
                                           'tunai','ra_order_dua.id_entitas as id_entitas')

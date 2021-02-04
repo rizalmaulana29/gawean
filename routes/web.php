ab6653b90fe844bf9c4364db5876b62b0992dcd3
sandi@niagateknologi.net <Sukses100>
2021-02-04 12:02:33 +0700

    live jurnal po

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 	$router->post('TestWA', 	['uses' => 'CartDevController@sendWa']);
 
 	$router->post('TestNotif', 	['uses' => 'CartDevController@notifTransaksi']);
-	$router->post('testing', 	['uses' => 'CartDevController@testing_aja']);
+	$router->get('testing', 	['uses' => 'CartDevController@testing_aja']);
 	
 	$router->post('stockTool', 	['uses' => 'ToolsController@toolsMath']);
 

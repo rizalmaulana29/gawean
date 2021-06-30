1e2deedc565d7481f83cb7dac0d639263c0f0f89
sandi@niagateknologi.net <Sukses100>
2021-06-30 11:36:12 +0700

    get data multiple Company

--- app/Http/Controllers/JurnalExspensesController.php
+++ app/Http/Controllers/JurnalExspensesController.php
@@ -12,7 +12,7 @@ use App\Pendapatan;
 use App\JurnalLog;
 use App\Paymeth;
 use App\AdminEntitas;
-use App\Anak;
+use App\Expenses;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
@@ -89,49 +89,27 @@ class JurnalExspensesController extends Controller
     }
 
 
-    private function createExpenses($getDataTransaksi){
+    private function getExpenses($getDataTransaksi){
       
       $tgl = strtotime($getDataTransaksi['tgl_transaksi']);
       $tglTransaksi = date('Y-m-d',$tgl);
 
-      $dataRaw = [
-                "expense"  => [ 
-                                        "refund_from_name"   => "Mandiri Publik 131 000 711 2586",
-                                        "person_name"        => "Nicepay",
-                                        "transaction_date"   => $tglTransaksi,
-                                        "payment_method_name"=> "Transfer Bank",
-                                        "payment_method_id"  => 792898,
-                                        "transaction_no"     => $getDataTransaksi['id_transaksi'],
-                                        "custom_id"          => $getDataTransaksi['id_transaksi'],
-                                        "expense_payable"    => false,
-                                        "transaction_account_lines_attributes"  => [[ "account_name" => "Biaya lain-lain",
-                                                                                      "description"  => "biaya nicepay 2011240027650",
-                                                                                      "debit"        => 4400
-                                                                                   ]],
-                                      ]
-                  ];
-
-
-      $encodedataRaw = json_encode($dataRaw);
-
       $curl = curl_init();
 
       curl_setopt_array($curl, array(
-        CURLOPT_URL            => "https://api.jurnal.id/core/api/v1/expenses",
+        CURLOPT_URL => "https://api.jurnal.id/core/api/v1/expenses",
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING       => "",
-        CURLOPT_MAXREDIRS      => 10,
-        CURLOPT_TIMEOUT        => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST  => "POST",
-        CURLOPT_POSTFIELDS     => $encodedataRaw,
-        CURLOPT_HTTPHEADER     => array(
-                                        "apikey: 56593d3e45a37eb7033e356d33fd83c4",
-                                        "Authorization: 815f1ce4f83e46a3a3f2b87ac79fc79c",
-                                        "Content-Type: application/json; charset=utf-8",
-                                        "Cookie: visid_incap_1892526=sSSXIkPcR2OGEG8EIsR1kvKfq18AAAAAQUIPAAAAAAAbLIHIENx0sm8jw/V3q49p; nlbi_1892526=8trIdrnO9S4KHtCQKezQ4QAAAAC1Ln3MtHQzDOiZP5/QXp4v; incap_ses_959_1892526=3SwTKPUwzU0bAScrnA1PDc+i0V8AAAAA2wbKV6ShlqO9SQ9NTtMN7g=="
-                                      ),
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_POSTFIELDS => "{}",
+        CURLOPT_HTTPHEADER => array(
+                                    "apikey: ".$jurnalKoneksi['jurnal_key'],
+                                    "Authorization: ".$jurnalKoneksi['jurnal_auth'],
+                                    "content-type: application/json"
+        ),
       ));
       $response = curl_exec($curl);
       $err = curl_error($curl);

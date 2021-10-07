75092f91adab61a50f138e05aa378df233bacc67
Mochamad Rivan <mrivan7799@gmail.com>
2021-10-07 16:38:38 +0700

    Test Yout Might Fee agen dan kantor

--- app/Http/Controllers/KeagenanController.php
+++ app/Http/Controllers/KeagenanController.php
@@ -27,8 +27,10 @@ class KeagenanController extends Controller
 
     public function cronKeagenan(Request $request){
         $getPayment = Payment::select("ra_payment_dua.id_transaksi","ra_payment_dua.id_agen","ra_payment_dua.nominal_total")
-                        ->selectRaw("(ra_payment_dua.nominal_total * b.angka)/100 nominal_fee")
+                        ->selectRaw("(ra_payment_dua.nominal_total * b.angka)/100 nominal_fee_agen")
+                        ->selectRaw("(ra_payment_dua.nominal_total * c.angka)/100 nominal_fee_kantor")
                         ->leftJoin("ra_setting_fee AS b","ra_payment_dua.id_agen","=", "b.id_users")
+                        ->leftJoin("ra_setting_fee AS c","ra_payment_dua.id_kantor","=", "c.id_users")
                         ->where("ra_payment_dua.status","paid")
                         ->where("ra_payment_dua.id_agen","!=","")
                         ->whereNotNull("ra_payment_dua.id_agen")
@@ -45,14 +47,15 @@ class KeagenanController extends Controller
             $hello[$key]["id_transaksi"]    = $value->id_transaksi;
             $hello[$key]["id_agen"]         = $value->id_agen;
             $hello[$key]["nominal_total"]   = $value->nominal_total;
-            $hello[$key]["nominal_fee"]     = $value->nominal_fee;
+            $hello[$key]["nominal_fee_agen"]     = $value->nominal_fee_agen;
+            $hello[$key]["nominal_fee_kantor"]     = $value->nominal_fee_kantor;
             $i++;
             
             // $savePencairanDetail    = new PencairanDetail;
             // $savePencairanDetail->id_transaksi  = $value->id_transaksi;
             // $savePencairanDetail->id_agen       = $value->id_agen;
             // $savePencairanDetail->nominal_total = $value->nominal_total;
-            // $savePencairanDetail->nominal_fee   = $value->nominal_fee;
+            // $savePencairanDetail->nominal_fee   = $value->nominal_fee_agen;
             // $savePencairanDetail->save();
             // b. insert hasil select tadi ke ra_pencairan_detail (id_transaksi, id_agen, nominal_total, nominal_fee) 
         

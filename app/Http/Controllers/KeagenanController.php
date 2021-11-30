37326d026fd2ebb2d86b1d66045c53cce82e47da
Mochamad Rivan <mrivan7799@gmail.com>
2021-11-30 10:15:30 +0700

    percobaan API keagenan controller

--- app/Http/Controllers/KeagenanController.php
+++ app/Http/Controllers/KeagenanController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 // use App\Nicepaylog;
@@ -23,34 +24,123 @@ class KeagenanController extends Controller
     public function __construct()
     {
         date_default_timezone_set("Asia/Jakarta");
-        echo "Start Time: ".Carbon::now();
+        echo "Start Time: " . Carbon::now();
         echo "<br>";
         $this->starttime = microtime(true);
     }
 
-    public function cronKeagenan(Request $request){
+    public function cronKeagenan(Request $request)
+    {
 
-        $getPayment = Payment::select("ra_payment_dua.id_transaksi","ra_payment_dua.id_agen","ra_payment_dua.id_kantor","ra_payment_dua.nominal_total","ra_payment_dua.tgl_transaksi","ra_payment_dua.tgl_kirim")
-                        ->selectRaw("(ra_payment_dua.nominal_total * b.angka)/100 nominal_fee_agen")
-                        ->selectRaw("(ra_payment_dua.nominal_total * c.angka)/100 nominal_fee_kantor")
-                        ->leftJoin("ra_setting_fee AS b","ra_payment_dua.id_agen","=", "b.id_users")
-                        ->leftJoin("ra_setting_fee AS c","ra_payment_dua.id_kantor","=", "c.id_users")
-                        ->where("ra_payment_dua.status","paid")
-                        ->where("ra_payment_dua.id_agen","!=","")
-                        ->whereNotNull("ra_payment_dua.id_agen")
-                        // ->where("ra_payment_dua.varian","Aqiqah")
-                        ->where("ra_payment_dua.tipe","transaksi")
-                        ->where("ra_payment_dua.lunas","y")
-                        ->whereNull("ra_payment_dua.hitung_fee")
-                        ->where("ra_payment_dua.nominal_total",">",0)
-                        ->where("b.jenis_fee","=","persentase")
-                        ->orderBy("ra_payment_dua.tgl_transaksi","ASC")
-                        ->limit(100)
-                        ->get();
+        // $getPayment = Payment::select("ra_payment_dua.id_transaksi","ra_payment_dua.id_agen","ra_payment_dua.id_kantor","ra_payment_dua.nominal_total","ra_payment_dua.tgl_transaksi","ra_payment_dua.tgl_kirim")
+        //                 ->selectRaw("(ra_payment_dua.nominal_total * b.angka)/100 nominal_fee_agen")
+        //                 ->selectRaw("(ra_payment_dua.nominal_total * c.angka)/100 nominal_fee_kantor")
+        //                 ->leftJoin("ra_setting_fee AS b","ra_payment_dua.id_agen","=", "b.id_users")
+        //                 ->leftJoin("ra_setting_fee AS c","ra_payment_dua.id_kantor","=", "c.id_users")
+        //                 ->where("ra_payment_dua.status","paid")
+        //                 ->where("ra_payment_dua.id_agen","!=","")
+        //                 ->whereNotNull("ra_payment_dua.id_agen")
+        //                 // ->where("ra_payment_dua.varian","Aqiqah")
+        //                 ->where("ra_payment_dua.tipe","transaksi")
+        //                 ->where("ra_payment_dua.lunas","y")
+        //                 ->whereNull("ra_payment_dua.hitung_fee")
+        //                 ->where("ra_payment_dua.nominal_total",">",0)
+        //                 ->where("b.jenis_fee","=","persentase")
+        //                 ->orderBy("ra_payment_dua.tgl_transaksi","ASC")
+        //                 ->limit(100)
+        //                 ->get();
 
-        echo "Jumlah Transaksi To Updated: ".$getPayment->count();
+        $getPayment = Payment::selectRaw("
+                    SELECT
+                    t.id_transaksi,
+                    t.id_agen,
+                    t.nominal_total,
+                    t.tgl, t.tgl_kirim,
+                    t.varian,
+                    t.nominal_fee,	t.name , t.label,
+                    t.created_at from 
+                    (
+                SELECT
+                    a.id_transaksi,
+                    a.id_agen,
+                    a.nominal_total,
+                    a.tgl, a.tgl_kirim,
+                    a.varian,
+                (a.nominal_total * b.angka)/100 nominal_fee,	c.name , concat('Biaya Fee Aqiqah Agen ',c.name,' ','ID ', a.id_transaksi) label,
+                    NOW() created_at
+                FROM
+                    ra_payment_dua a 
+                    left join ra_setting_fee b on a.id_agen = b.id_users
+                    left join cms_users c ON a.id_agen = c.id
+                WHERE
+                    a.STATUS = 'paid' 
+                    AND a.id_agen != '' 
+                    AND a.id_agen != 'null' 
+                    AND a.varian = 'Aqiqah' 
+                    AND b.varian = 'Aqiqah' 
+                    AND a.tipe = 'transaksi' 
+                    AND a.lunas = 'y' 
+                    AND a.hitung_fee IS NULL
+                    AND a.nominal_total > 0
+                    AND b.jenis_fee = 'persentase'
+                
+                UNION 
+                    
+                    SELECT
+                    a.id_transaksi,
+                    a.id_kantor id_agen,
+                    a.nominal_total,
+                    a.tgl, a.tgl_kirim,
+                    a.varian,
+                (a.nominal_total * b.angka)/100 nominal_fee	, c.name , concat('Biaya Fee Aqiqah SO ',c.name,' ','ID ', a.id_transaksi) label,
+                    NOW() created_at
+                FROM
+                    ra_payment_dua a 
+                    left join ra_setting_fee b on a.id_kantor = b.id_users
+                    left join cms_users c ON a.id_kantor = c.id
+                WHERE
+                    a.STATUS = 'paid' 
+                    AND a.id_agen != '' 
+                    AND a.id_agen != 'null' 
+                    AND a.varian = 'Aqiqah' 
+                    AND b.varian = 'Aqiqah' 
+                    AND a.tipe = 'transaksi' 
+                    AND a.lunas = 'y' 
+                    AND a.hitung_fee IS NULL
+                    AND a.nominal_total > 0
+                    AND b.jenis_fee = 'persentase'
+                    
+                    UNION 
+                    
+                    SELECT
+                    a.id_transaksi,
+                    a.id_kantor id_agen,
+                    a.nominal_total,
+                    a.tgl, a.tgl_kirim,
+                    a.varian,
+                (a.nominal_total * b.angka)/100 nominal_fee	, c.name,  concat('Biaya Fee Retail_Food SO ',c.name,' ','ID ', a.id_transaksi) label,
+                    NOW() created_at 
+                FROM
+                    ra_payment_dua a 
+                    left join ra_setting_fee b on a.id_kantor = b.id_users
+                    left join cms_users c ON a.id_kantor = c.id
+                WHERE
+                    a.STATUS = 'paid' 
+                    AND a.id_agen != '' 
+                    AND a.id_agen != 'null' 
+                    AND a.varian = 'Retail_Food' 
+                    AND b.varian = 'Retail_Food' 
+                    AND a.tipe = 'transaksi' 
+                    AND a.lunas = 'y' 
+                    AND a.hitung_fee IS NULL
+                    AND a.nominal_total > 0
+                    AND b.jenis_fee = 'persentase' ) t")
+            ->get();
+
+        echo "Jumlah Transaksi To Updated: " . $getPayment->count();
         echo "<br>";
         echo "<br>";
+        /*
         foreach($getPayment as $key => $value){
             $tgl_transaksi = Carbon::createFromFormat('Y-m-d H:i:s', $value->tgl_transaksi)->format("Y-m-d");
             echo "Id Transaksi : ".$value->id_transaksi;
@@ -67,14 +157,14 @@ class KeagenanController extends Controller
             $savePencairanDetail->tgl_kirim     = $value->tgl_kirim;
             $savePencairanDetail->save();
             
-            $savePencairanDetailKantor    = new PencairanDetail;
-            $savePencairanDetailKantor->id_transaksi  = $value->id_transaksi;
-            $savePencairanDetailKantor->id_agen       = $value->id_kantor;
-            $savePencairanDetailKantor->nominal_total = $value->nominal_total;
-            $savePencairanDetailKantor->nominal_fee   = $value->nominal_fee_kantor;
-            $savePencairanDetailKantor->tgl_transaksi = $tgl_transaksi;
-            $savePencairanDetailKantor->tgl_kirim     = $value->tgl_kirim;
-            $savePencairanDetailKantor->save();
+            // $savePencairanDetailKantor    = new PencairanDetail;
+            // $savePencairanDetailKantor->id_transaksi  = $value->id_transaksi;
+            // $savePencairanDetailKantor->id_agen       = $value->id_kantor;
+            // $savePencairanDetailKantor->nominal_total = $value->nominal_total;
+            // $savePencairanDetailKantor->nominal_fee   = $value->nominal_fee_kantor;
+            // $savePencairanDetailKantor->tgl_transaksi = $tgl_transaksi;
+            // $savePencairanDetailKantor->tgl_kirim     = $value->tgl_kirim;
+            // $savePencairanDetailKantor->save();
             # b. insert hasil select tadi ke ra_pencairan_detail (id_transaksi, id_agen, nominal_total, nominal_fee) 
         
             $updatePayment    = Payment::where("id_transaksi",$value->id_transaksi)->first();
@@ -82,12 +172,13 @@ class KeagenanController extends Controller
             $updatePayment->save();
             # c. update ra_payment_dua set hitung_fee = y where id_transaksi IN (transaksi yg poin a tadi di atas )
         }
+        */
 
         $endtime = microtime(true);
         $timediff = $endtime - $this->starttime;
-        echo "End Time ".Carbon::now();
+        echo "End Time " . Carbon::now();
         echo "<br>";
-        echo "Elapsed Time : ". $this->secondsToTime($timediff) ." In Microtime : " .ROUND(($timediff), 4);
+        echo "Elapsed Time : " . $this->secondsToTime($timediff) . " In Microtime : " . ROUND(($timediff), 4);
         echo "<br>";
         die();
     }
@@ -99,7 +190,6 @@ class KeagenanController extends Controller
         $s -= $h * 3600;
         $m = floor($s / 60);
         $s -= $m * 60;
-        return $h.':'.sprintf('%02d', $m).':'.sprintf('%02d', $s);
+        return $h . ':' . sprintf('%02d', $m) . ':' . sprintf('%02d', $s);
     }
-
 }

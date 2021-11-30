ddd2d6ce16eac6491f98486730e44462eca0f82c
Mochamad Rivan <mrivan7799@gmail.com>
2021-11-30 10:50:16 +0700

    Keagenan test Lagi

--- app/Http/Controllers/KeagenanController.php
+++ app/Http/Controllers/KeagenanController.php
@@ -24,7 +24,7 @@ class KeagenanController extends Controller
     public function __construct()
     {
         date_default_timezone_set("Asia/Jakarta");
-        echo "Start Time: Test " . Carbon::now();
+        echo "Start Time Hallo: " . Carbon::now();
         echo "<br>";
         $this->starttime = microtime(true);
     }
